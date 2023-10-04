c16c525b98e7ad750d3f59e80584ced1a1ed7d2f
hamDrambus <hamDrambus@github.com>
2023-10-04 12:42:05 +0700

    Prettied up python code according to colleague's review.

--- scripts/CaenDummyEventViewerQt/eventview.py
+++ scripts/CaenDummyEventViewerQt/eventview.py
@@ -23,13 +23,14 @@ import numpy as np
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-        uic.loadUi("scripts/CaenDummyEventViewerQt/EventViewer.ui", self)
-        self.plot1 = self.graphicsView.addPlot(row=0, col=0)
-        self.plot2 = self.graphicsView.addPlot(row=1, col=0)
-        self.plot1.setXLink(self.plot2)
-        self.lFileNumber.setReadOnly(True)
-        self.lEventNumber.setReadOnly(True)
-        self.bNextEvent.clicked.connect(self.next_event)
+        ui = uic.loadUi("scripts/CaenDummyEventViewerQt/EventViewer.ui", self)
+        ui.plot1 = ui.graphicsView.addPlot(row=0, col=0)
+        ui.plot2 = ui.graphicsView.addPlot(row=1, col=0)
+        ui.plot1.setXLink(ui.plot2)
+        ui.le_file_number.setReadOnly(True)
+        ui.le_event_number.setReadOnly(True)
+        ui.pb_next_event.clicked.connect(self.next_event)
+        self.ui = ui
         self.showMaximized()
 
         self.data_folder = "/home/fvirt/Documents/AMS_DAQ/DAQling/daqling-develop/tmp/"
@@ -37,127 +38,110 @@ class MainWindow(QMainWindow):
         self.file_pattern = "test-2023-10-02-[0-2][0-9]:[0-5][0-9]:[0-5][0-9].[0-9].[0-9].*.txt"
         
         try:
-            self.current_filenum = int(self.lFileNumber.text())
+            self.current_filenum = int(self.ui.le_file_number.text())
         except ValueError:
             self.current_filenum = None
-            self.statusbar.showMessage("Warning: Invalid file number '" + self.lFileNumber.text() + "'!")
+            self.ui.statusbar.showMessage("Warning: Invalid file number '" + self.ui.le_file_number.text() + "'!")
         self.current_file = self.find_file(self.current_filenum)
         #print("current_file = '" + self.current_file.resolve().as_posix() if self.current_file is not None else "None" + "'")
-        self.opened_file = None
+        self.current_file_content = None
         self.event_data = None
         self.next_event()
 
     def update_file_num(self):
-        self.current_filenum = self.get_file_number(self.current_file)
-        if self.current_filenum is None:
-            self.lFileNumber.setText("None")
+        fn = self.current_filenum = self.get_file_number(self.current_file)
+        if fn is None:
+            self.ui.le_file_number.setText("None")
         else:
-            self.lFileNumber.setText(str(self.current_filenum))
-        if self.event_data is None or "event_number" not in self.event_data:
-            self.lEventNumber.setText("None")
+            self.ui.le_file_number.setText(str(fn))
+        data = self.event_data
+        if data is None or "event_number" not in data:
+            self.ui.le_event_number.setText("None")
         else:
-            self.lEventNumber.setText(str(self.event_data["event_number"]))
+            self.ui.le_event_number.setText(str(data["event_number"]))
 
 
-    def next_file(self, filelist, last_file):
-        if self.opened_file is not None:
-            self.opened_file.close()
-            self.opened_file = None
+    def next_file(self, current_file, filelist, last_file):
         if filelist is None or len(filelist) == 0:
-            self.statusbar.showMessage("Warning: No data files were found!")
+            self.ui.statusbar.showMessage("Warning: No data files were found!")
             return None
         try:
-            index = filelist.index(self.current_file)
+            index = filelist.index(current_file)
             index += 1
             if index == len(filelist):
                 index = 0
             if last_file == filelist[index]:
-                self.statusbar.showMessage("Error: Could not open any of the files!")
+                self.ui.statusbar.showMessage("Error: Could not open any of the files!")
                 return None
             return filelist[index]            
         except ValueError:
             file = self.find_file(self.current_filenum+1)
             if file is None:
                 return filelist[0]
-            
-    def open_current_file(self):
-        if self.opened_file is None:
-            try:
-                self.opened_file = open(self.current_file, "r")
-                return True
-            except OSError:
-                self.opened_file = None
-                self.statusbar.showMessage("Error: Could not read file '" + self.current_file.resolve().as_posix() + "'! Going to the next one.")
-                return False
-        return True
+            return file
         
     def next_event(self):
+        current_file = self.current_file
         last_file = self.current_file # To avoid endless loop.
         file_list = self.get_file_list()
 
-        if self.current_file is None:
-            self.current_file = self.next_file(file_list, last_file)
-        
-        while self.opened_file is None and self.current_file is not None:
-            if not self.open_current_file():
-               self.current_file = self.next_file(file_list, last_file)
+        if current_file is None or not self.current_file_content: 
+            current_file = self.next_file(current_file, file_list, last_file)
+            while current_file:
+                with open(current_file, "r") as file:
+                    self.current_file_content = file.readlines()
+                    break
+                self.ui.statusbar.showMessage("Error: Could not read file '" + current_file.resolve().as_posix() + "'! Going to the next one.")
+                current_file = self.next_file(current_file, file_list, last_file)
+        self.current_file = current_file
 
-        if self.current_file is None:
+        if current_file is None:
             self.update_file_num()
             return
 
-        #print("Opened file '" + self.current_file.resolve().as_posix() + "'")
-        line = self.opened_file.readline()
-        if not line and last_file != self.current_file:
-            self.statusbar.showMessage("Warning: Empty file '" + self.current_file.resolve().as_posix()+ "'! No new event.")
-            self.update_file_num()
-            self.current_file = self.next_file(file_list, last_file)
-            return
-        
-        if not line: # All events in file have ended
-            self.current_file = self.next_file(file_list, last_file)
-            # self.next_event() # If file is empty it will skip it instead of printing a warning
-            while self.opened_file is None and self.current_file is not None:
-                if not self.open_current_file():
-                    self.current_file = self.next_file(file_list, last_file)
-            if self.current_file is None:
-                self.update_file_num()
-                return
-            #print("Opened file '" + self.current_file.resolve().as_posix() + "'")
-            line = self.opened_file.readline()
+        self.event_data = self.read_event(self.current_file_content)
+        self.plot_event(self.event_data)
+        self.update_file_num()
 
-        # Read event from file and plot
-        # TODO: to separate function
-        self.plot1.clear()
-        self.plot2.clear()
+    # Read event from file lines
+    def read_event(self, file_content):
+        event_data = {}
         try:
-            self.event_data = {}
-            self.event_data["event_number"] = int(line.rstrip())
-            line = self.opened_file.readline()
-            self.event_data["event_timestamp"] = int(line.rstrip())
-            line = self.opened_file.readline()
-            self.event_data["event_device"] = line
-            self.event_data["data"] = {}
+            line = file_content.pop(0)
+            event_data["event_number"] = int(line.rstrip())
+            line = file_content.pop(0)
+            event_data["event_timestamp"] = int(line.rstrip())
+            line = file_content.pop(0)
+            event_data["event_device"] = line
+            event_data["data"] = {}
             while True:
-                line = self.opened_file.readline()
+                line = file_content.pop(0)
                 if not line.rstrip():
                     break
                 channel = int(line.rstrip())
-                line = self.opened_file.readline()
+                line = file_content.pop(0)
                 xs = np.fromstring(line.rstrip(), dtype=np.int_, sep=" ")
-                line = self.opened_file.readline()
+                line = file_content.pop(0)
                 ys = np.fromstring(line.rstrip(), dtype=np.int_, sep=" ")
-                self.event_data["data"][channel] = (xs, ys)            
-            if 0 in self.event_data["data"]:
-                self.plot1.plot(self.event_data["data"][0][0], self.event_data["data"][0][1])
-            if 1 in self.event_data["data"]:
-                self.plot2.plot(self.event_data["data"][1][0], self.event_data["data"][1][1])
-            self.statusbar.showMessage("OK")
-        except ValueError:
-            self.statusbar.showMessage("Warning: invalid data in '" + self.current_file.resolve().as_posix() + "'!")
-            self.event_data = None
-        self.update_file_num()
+                event_data["data"][channel] = (xs, ys)
+        except (ValueError, IndexError) as e:
+            self.ui.statusbar.showMessage("Warning: invalid data in '" + self.current_file.resolve().as_posix() + "'!")
+            event_data = None
+        return event_data
 
+    def plot_event(self, event_data):
+        self.ui.plot1.clear()
+        self.ui.plot2.clear()
+        if not event_data:
+            return
+        try:
+            if 0 in event_data["data"]:
+                self.ui.plot1.plot(event_data["data"][0][0], event_data["data"][0][1])
+            if 1 in event_data["data"]:
+                self.ui.plot2.plot(event_data["data"][1][0], event_data["data"][1][1])
+            self.ui.statusbar.showMessage("OK")
+        except ValueError as e:
+            self.ui.statusbar.showMessage("Warning: invalid data in '" + self.current_file.resolve().as_posix() + "'!")
 
     @staticmethod
     def get_file_number(file):
@@ -168,9 +152,10 @@ class MainWindow(QMainWindow):
             return int(res[-2])
         
     def find_file(self, filenum):
+        filelist = self.get_file_list()
         if filenum is None:
-            return self.get_file_list()[0]
-        for f in self.get_file_list():
+            return filelist[0]
+        for f in filelist:
             if self.get_file_number(f) == filenum:
                 return f
         return None
